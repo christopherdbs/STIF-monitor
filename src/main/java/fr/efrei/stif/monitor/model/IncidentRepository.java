50430b17d873918ea40e66b272a63d4bca79f0dc
christopherdbs <christopherdb77@gmail.com>
2026-04-02 12:21:06 +0200

    feat: add company suggestions, filter active incidents by datetime and change no assigned company icons

--- src/main/java/fr/efrei/stif/monitor/model/IncidentRepository.java
+++ src/main/java/fr/efrei/stif/monitor/model/IncidentRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IncidentRepository extends JpaRepository<IncidentReport, Integer>{
-    List<IncidentReport> findByStatusFalse();
+    List<IncidentReport> findByStatusFalseOrderByDateTimeAsc();
     List<IncidentReport> findByEquipmentIdOrderByDateTimeDesc(Integer equipmentId);
     List<IncidentReport> findTop3ByEquipmentIdAndIdNotOrderByDateTimeDesc(Integer equipmentId, Integer reportId);
 }
